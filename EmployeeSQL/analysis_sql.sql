c760ed999790eea0567076c679c12f3127ee72dc
Radhika Anusuri <rad.guttu@gmail.com>
2022-02-26 09:17:01 -0500

    complef all queries

--- EmployeeSQL/analysis_sql.sql
+++ EmployeeSQL/analysis_sql.sql
@@ -25,3 +25,36 @@ from employees e
 join dept_emp de on de.emp_no = e.emp_no
 join departments dp on dp.dept_no = de.dept_no
 
+--Query 5: List first name, last name, and sex for employees whose first name is "Hercules" and last names begin with "B."
+select e.first_name, e.last_name, e.sex
+from employees e
+where e.first_name = 'Hercules' and e.last_name like 'B%'
+
+--Query 6: List all employees in the Sales department, including their employee number, last name, first name, and department name.
+
+select e.emp_no, e.last_name, e.first_name, dp.dept_name
+from employees e
+join dept_emp de on de.emp_no = e.emp_no
+join departments dp on de.dept_no = dp.dept_no  and dp.dept_name = 'Sales'
+
+
+--Query 7 : List all employees in the Sales and Development departments, including their employee number, last name, 
+--first name, and department name.
+select e.emp_no, e.last_name, e.first_name, dp.dept_name
+from employees e
+join dept_emp de on de.emp_no = e.emp_no
+join departments dp on de.dept_no = dp.dept_no  and dp.dept_name IN ('Sales','Development')
+
+--Query 7;  with OR
+select e.emp_no, e.last_name, e.first_name, dp.dept_name
+from employees e
+join dept_emp de on de.emp_no = e.emp_no
+join departments dp on de.dept_no = dp.dept_no  and (dp.dept_name = 'Sales' OR dp.dept_name = 'Development')  
+
+--In descending order, list the frequency count of employee last names, i.e., how many employees share each last name.
+select last_name, count(last_name) as Emp_count 
+from employees 
+group by last_name
+order by Emp_count desc
+
+
